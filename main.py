9518af6fc23d2bb795515148740d5d569c986cad
Chitra <93026980+chitra-08@users.noreply.github.com>
2022-02-11 23:27:29 +0530

    Add files via upload
    Enhanced LogisticRegression Module put up

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pickle
 import sklearn
 
 app = Flask(__name__)
-model = pickle.load(open("loanApplicant_lr.pkl", "rb"))
+model = pickle.load(open("loanApplicant_lr1.pkl", "rb"))
 loanStatus = ''
 
 
@@ -12,9 +12,11 @@ def thankyou(status):
     print(status)
     return render_template('ThankYou.html', status=status)
 
+
 @app.route('/')
 def home():
     return render_template('LoanApplication.html')
+
 
 @app.route('/apply', methods=['GET', 'POST'])
 def apply():
@@ -24,19 +26,18 @@ def apply():
         age = float(request.form.get('age'))
         inc = float(request.form.get('income'))
         loan_amt = float(request.form.get('loanamt'))
+        prop_value = float(request.form.get('propVal'))
         if request.form['cr_hist'] is not None or request.form['cr_hist'] != 'NA':
             credit_history = int(request.form['cr_hist'])
         if request.form['prop_type'] is not None or request.form['prop_type'] != 'NA':
             prop_type = int(request.form['prop_type'])
-        prop_valuation = 700000
-        lmi = loan_amt / prop_valuation
         prediction = model.predict([[
             age,
             inc,
             loan_amt,
             credit_history,
             prop_type,
-            lmi
+            prop_value
         ]])
         print(prediction)
         output = round(prediction[0])
@@ -49,5 +50,5 @@ def apply():
 
     return render_template('LoanApplication.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+app.run(debug=True)
